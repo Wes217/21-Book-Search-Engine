4f811128909d4f9b0ce4e899cb311d0ad399121b
Lendo <wesreedbusiness@gmail.com>
2023-08-21 20:55:18 -0500

    readme

---
+++ README.md
@@ -0,0 +1,4 @@
+# 20-Book-Search-Engine
+
+# description
+A search engine to lookup and save books you like.
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client'
 
-export const QUEY_ME = gql`
+export const GET_ME = gql`
 query Query {
     me {
       _id
